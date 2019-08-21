1604eefc79f305f81dfb07c641c8f1dfb50146bd
Chamith Siriwardena <chamith@gmail.com>
2019-08-21 14:18:43 +0530

    adding CORS settings

--- src/main/java/org/testunited/core/web/TestCaseController.java
+++ src/main/java/org/testunited/core/web/TestCaseController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import org.testunited.core.TestTarget;
 import org.testunited.core.services.TestCaseService;
 
 @RestController
+@CrossOrigin
 public class TestCaseController {
 
 	@Autowired
